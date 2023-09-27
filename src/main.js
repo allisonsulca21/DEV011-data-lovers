1d81afe0d0bd6d5057c96b5dbf5ef6c9a5735abe
Allison Sulca <asulca2112@gmail.com>
2023-09-27 07:54:21 -0500

    cartas mostradas y estilos actualizados

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, renderItems, data);
+console.log( data.pokemon);
+const allCards = renderItems(data.pokemon)
+const root = document.querySelector("#root");
+root.appendChild(allCards);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,15 @@
-// estas funciones son de ejemplo
-
 export const renderItems = (data) => {
-  return data;
+  const ul = document.createElement("ul")
+data.forEach(element => {
+  let cardLi = `
+    <div class="img">
+    <img src="${element.img}" />
+    </div>
+    <div class="name">${element.name} </div>
+`
+let li = document.createElement('li');
+    li.innerHTML = cardLi;
+    ul.append(li);
+});
+  return ul;
 };
-
